ea4e5e034a8fa6f652310e103da6aa2f728f469d
Aamir ali <msaamirali123@gmail.com>
2021-09-05 21:33:37 +0530

    Manage Expenses & Claim Form

--- server/index.js
+++ server/index.js
@@ -67,7 +67,9 @@ app.post('/login',(req,res)=>{
                     rep_ID: result[0].rep_ID,
                     manager_ID : result[0].manager_ID,
                 })
-              }     
+              } else {
+                  res.send({message : 'Invalid Credentials...!'})
+              } 
             }); 
 });  
 
@@ -851,8 +853,7 @@ app.get('/ViewCategory', (_req, res) =>{
 
 
 
-
-
+  
 
 
 
@@ -929,7 +930,7 @@ app.post("/applyLeave",(req,res)=>{
 //         res.send("Successfull");
 //     })
 //     console.log ("Error");
-// });         
+// });           
 
 app.post('/viewApprovedLeaves',(req,res)=>{
     const rep_ID = req.body.rep_ID;
@@ -994,34 +995,91 @@ app.post('/ManageLeaves/ViewPending',(req,res)=>{
     });
 });
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/* Manage Expenses */
+/* Manage Expenses */  
 
-app.post('/claimExpenses',(req,res)=>{
-    console.log(req)
+app.post('/ClaimExpenses',(req,res)=>{
+    // console.log(req)
     const rep_ID = req.body.rep_ID;
     const expense_Type = req.body.expense_Type;
+    const date = req.body.date;
     const location = req.body.location;
-    const bills = req.body.bills;
+    const bills = req.body.bills;  
     const amount = req.body.amount;
     const description = req.body.description;
     
-    const sqlClaimExpense = "INSERT INTO expenses (rep_ID,expense_Type,location,bills,amount,description) VALUES (?,?,?,?,?,?)";
+    const sqlClaimExpense = "INSERT INTO expenses (rep_ID,expense_Type,date,location,bills,amount,description) VALUES (?,?,?,?,?,?,?)";
 
-    db.query(sqlClaimExpense, [rep_ID,expense_Type,location,bills,amount,description], (err,result)=>{
+    db.query(sqlClaimExpense, [rep_ID,expense_Type,date,location,bills,amount,description], (err,result)=>{
         if(err){
             console.log(err);
-            console.log ("Error");
-        } else{
+            console.log ("Error while claim expenses");
+        } else{  
             // console.log("Expense claim submitted");
             res.send(result);
         }
-    })
+    })  
 });        
   
 
-app.get('/ViewCategory', (_req, res) =>{
+app.post('/ViewExpensesByCategory', (req, res) =>{
+    const rep_ID = req.body.rep_ID;
     //total expenses by category
-    db.query('SELECT expense_Type, (SUM(amount)) AS Total FROM expenses GROUP BY expense_Type', (err, result, _fields)=> {
+    db.query('SELECT expense_Type, (SUM(amount)) AS Total FROM expenses WHERE rep_ID = ? GROUP BY expense_Type', [rep_ID], (err, result, _fields)=> {
+      if(!err){
+        res.send(result);
+    }else {
+    console.log(err);  
+    }
+  });
+});
+app.post('/Expenses/DailyBatta', (req, res) =>{
+    const rep_ID = req.body.rep_ID;
+    //total expenses by category
+    db.query('SELECT (SUM(amount)) AS Total FROM expenses WHERE rep_ID = ? AND expense_Type="Daily batta"', [rep_ID], (err, result, _fields)=> {
+      if(!err){
+        res.send(result);
+    }else {
+    console.log(err);  
+    }
+  });
+});
+app.post('/Expenses/Accomodation', (req, res) =>{
+    const rep_ID = req.body.rep_ID;
+    //total expenses by category
+    db.query('SELECT (SUM(amount)) AS Total FROM expenses WHERE rep_ID = ? AND expense_Type="Accommodation"', [rep_ID], (err, result, _fields)=> {
+      if(!err){
+        res.send(result);
+    }else {
+    console.log(err);  
+    }
+  });
+});
+app.post('/Expenses/Fuel', (req, res) =>{
+    const rep_ID = req.body.rep_ID;
+    //total expenses by category
+    db.query('SELECT (SUM(amount)) AS Total FROM expenses WHERE rep_ID = ? AND expense_Type="Fuel"', [rep_ID], (err, result, _fields)=> {
+      if(!err){
+        res.send(result);
+    }else {
+    console.log(err);  
+    }
+  });
+});
+app.post('/Expenses/Other', (req, res) =>{
+    const rep_ID = req.body.rep_ID;
+    //total expenses by category
+    db.query('SELECT (SUM(amount)) AS Total FROM expenses WHERE rep_ID = ? AND expense_Type="Other"', [rep_ID], (err, result, _fields)=> {
+      if(!err){
+        res.send(result);
+    }else {
+    console.log(err);  
+    }
+  });
+});
+app.post('/Expenses/Total', (req, res) =>{
+    const rep_ID = req.body.rep_ID;
+    //total expenses by category
+    db.query('SELECT (SUM(amount)) AS Total FROM expenses WHERE rep_ID = ?', [rep_ID], (err, result, _fields)=> {
       if(!err){
         res.send(result);
     }else {
@@ -1030,9 +1088,10 @@ app.get('/ViewCategory', (_req, res) =>{
   });
 });
 
-app.get('/viewClaims',(_req,res)=>{
 
-    db.query('SELECT expense_ID, expense_Type, amount, description, date FROM expenses ORDER BY expense_ID DESC',(err,result,_fields)=>{
+app.post('/ViewExpenses',(req,res)=>{
+    const rep_ID = req.body.rep_ID;
+    db.query('SELECT * FROM expenses WHERE rep_ID=? ORDER BY date DESC',[rep_ID],(err,result,_fields)=>{
         if(!err){
             res.send(result);
         }else{
--- src/screens/ManageExpenses.js
+++ src/screens/ManageExpenses.js
@@ -1,47 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import { Text, View, SafeAreaView, ScrollView, StatusBar, Image, StyleSheet, AsyncStorage, Button} from 'react-native'
-import Styles from '../core/Styles'
-import TopNav from '../components/TopNav'
+import { Text, View, SafeAreaView, ScrollView, StatusBar, Image, StyleSheet, AsyncStorage} from 'react-native'
+import { Button, Card, DataTable } from 'react-native-paper'
 import { theme } from '../core/theme'
-import Icon from 'react-native-vector-icons/Ionicons'
+// import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5'
 import FontistoIcon from 'react-native-vector-icons/Fontisto'
 import { ThemeProvider } from '@react-navigation/native'
 import BackgroundLayout from '../components/BackgroundLayout'
-import {Card} from 'react-native-paper';
-import { DataTable } from 'react-native-paper';
 import axios from 'axios';
 
 const optionsPerPage = [2, 3, 4];
 
 export default function ManageExpenses({ navigation }) {
-
-  const [searchQuery, setSearchQuery] = useState('');
-  const onChangeSearch = query => { setSearchQuery(query) }
-  console.log(searchQuery);
-
-  const [page, setPage] = useState(3);
-  const [itemsPerPage, setItemsPerPage] = useState(optionsPerPage[0]);
-
-  const [expensesList,setExpensesList]=useState([]);
-
-  const [category, setCategory] = useState({
-    dailybatta : '',
-    fuel : '',
-    accomodations : '',
-    other : '',
-    total : '',
-})
-
-  useEffect(()=>{
-    axios.get("http://10.0.2.2:3001/viewClaims").then((response)=>{
-      setExpensesList(response.data);
-    })
-  },[]);
-
-  useEffect(() => {
-    setPage(0);
-  }, [itemsPerPage]);
 
   const [user, setUser] = useState({ rep_ID: '',  manager_ID: '',});
   
@@ -58,27 +29,111 @@ export default function ManageExpenses({ navigation }) {
       }
     }
     fetchData();
+  },[]);
+
+  const [searchQuery, setSearchQuery] = useState('');
+  const onChangeSearch = query => { setSearchQuery(query) }
+  console.log(searchQuery);
+
+  const [page, setPage] = useState(3);
+  const [itemsPerPage, setItemsPerPage] = useState(optionsPerPage[0]);
+
+  const [expensesList,setExpensesList]=useState([]);
+
+  const [dailybatta, setDailyBatta] = useState(0);
+  const [fuel, setFuel] = useState(0);
+  const [accomodations, setAccomodation] = useState(0);
+  const [other, setOther] = useState(0);
+  const [total, setTotal] = useState(0);
+
+
+  useEffect(()=>{
+    axios.post("http://10.0.2.2:3001/ViewExpenses",{
+      rep_ID : user.rep_ID,
+    }).then((response)=>{
+      setExpensesList(response.data);
+    });
   },[user]);
+
+  useEffect(() => {
+    setPage(0);
+  }, [itemsPerPage]);
+
 
   useEffect(() => {
     async function fetchData(){
       try {
-        axios.post("http://10.0.2.2:3001/ViewCategory",{
-            expense_ID : expense_ID,  
+        axios.post("http://10.0.2.2:3001/Expenses/DailyBatta",{
+            rep_ID : user.rep_ID,  
           }).then((response)=>{
-            setCategory({...category,
-              dailybatta : response.data[0].dailybatta,
-              accomodations : response.data[0].accomodations,
-              fuel : response.data[0].fuel,
-              other : response.data[0].other,      
-          });
+            setDailyBatta(response.data[0].Total);
           });
       } catch (e){
         console.log(e);
       }
     }
     fetchData();
-  },[]);
+  },[dailybatta]);
+
+  useEffect(() => {
+    async function fetchData(){
+      try {
+        axios.post("http://10.0.2.2:3001/Expenses/Accomodation",{
+          rep_ID : user.rep_ID,  
+        }).then((response)=>{
+          setAccomodation(response.data[0].Total);
+        });
+      } catch (e){
+        console.log(e);
+      }
+    }
+    fetchData();
+  },[accomodations]);
+
+  useEffect(() => {
+    async function fetchData(){
+      try {
+        axios.post("http://10.0.2.2:3001/Expenses/Fuel",{
+          rep_ID : user.rep_ID,  
+        }).then((response)=>{
+          setFuel(response.data[0].Total);
+        });
+      } catch (e){
+        console.log(e);
+      }
+    }
+    fetchData();
+  },[fuel]);
+
+  useEffect(() => {
+    async function fetchData(){
+      try {
+        axios.post("http://10.0.2.2:3001/Expenses/Other",{
+          rep_ID : user.rep_ID,  
+        }).then((response)=>{
+          setOther(response.data[0].Total);
+        });
+      } catch (e){
+        console.log(e);
+      }
+    }
+    fetchData();
+  },[other]);
+
+  useEffect(() => {
+    async function fetchData(){
+      try {
+        axios.post("http://10.0.2.2:3001/Expenses/Total",{
+          rep_ID : user.rep_ID,  
+        }).then((response)=>{
+          setTotal(response.data[0].Total);
+        });;
+      } catch (e){
+        console.log(e);
+      }
+    }
+    fetchData();
+  },[total]);
   
 
   const ViewExpense = (expense_ID) => {
@@ -91,43 +146,63 @@ export default function ManageExpenses({ navigation }) {
       <ScrollView> 
       <BackgroundLayout>
 
-      <View style = {styles.sameRow}>
-      <Text></Text>
-        <Button color = '#0A6466' title = 'claim for expenses' onPress={() => navigation.navigate('ClaimExpenses')}>Claim</Button>
+        <View style={{flexDirection : "row", alignSelf:'flex-end'}}>
+            <Button
+              mode='contained'
+              icon={({color, size}) => (
+                  <FontAwesome5Icon
+                  name="wpforms" 
+                  color= {theme.colors.surface}
+                  size={25}
+                  />
+              )}
+              onPress={() => navigation.navigate('ClaimExpenses')} 
+              // labelStyle = {{fontWeight : 'bold', marginLeft : -2, color:'#0000FF'}}
+              > Claim For Expenses
+            </Button>
+        </View>
 
-      </View>
+
       
       <View>
       
       <Card style={ styles.card }>
-        <Card.Title title="Total Expenses by Category"/>
-        <View style={{ marginBottom : 20 }}></View>
+        <Card.Title title="Total Expenses by Category" style={{}} />
         <Card.Content >
-          <View style={{  marginBottom : 4 }}></View>
           <View style= {styles.sameRow}>
+              <FontAwesome5Icon name="money-bill-wave" size={22} color={theme.colors.primary} />
               <Text style={styles.textLable}>Daily Batta : </Text>
-              <Text style={styles.text}>Rs. {category.dailybatta}.00</Text>
+              <Text style={styles.text}>Rs. {dailybatta}.00</Text>
           </View>
           <View style= {styles.sameRow}>
+              <FontAwesome5Icon name="hotel" size={22} color={theme.colors.primary} />
               <Text style={styles.textLable}>Accomodations : </Text>
-              <Text style={styles.text}>Rs. {category.accomodations}.00</Text>
+              <Text style={styles.text}>Rs. {accomodations}.00</Text>
           </View>
           <View style= {styles.sameRow}>
+              <Icon name="fuel" size={25} color={theme.colors.primary} />
               <Text style={styles.textLable}>Fuel : </Text>
-              <Text style={styles.text}>Rs. {category.fuel}.00</Text>
+              <Text style={styles.text}>Rs. {fuel}.00</Text>
           </View>
           <View style= {styles.sameRow}>
+              <FontAwesome5Icon name="coins" size={22} color={theme.colors.primary} />
               <Text style={styles.textLable}>Other : </Text>
-              <Text style={styles.text}>Rs. {category.other}.00</Text>
+              <Text style={styles.text}>Rs. {other}.00</Text>
+          </View>
+          <View style= {styles.sameRow}>
+              <FontAwesome5Icon name="money-check-alt" size={22} color={theme.colors.primary} />
+              <Text style={styles.textLable}>Total : </Text>
+              <Text style={styles.text}>Rs. {total}.00</Text>
           </View>
         </Card.Content>
       </Card>
 
        <DataTable>
             <DataTable.Header>
-              <DataTable.Title >Type of Expense</DataTable.Title>
-              <DataTable.Title >Amount</DataTable.Title>
-              <DataTable.Title >Date</DataTable.Title>
+              <DataTable.Title >Expense Type</DataTable.Title>
+              <DataTable.Title > |  Amount  | </DataTable.Title>
+              <DataTable.Title >Date | </DataTable.Title>
+              <DataTable.Title numeric>Status</DataTable.Title>
             </DataTable.Header>
 
             {expensesList.filter(val=>{if(searchQuery===""){
@@ -142,13 +217,36 @@ export default function ManageExpenses({ navigation }) {
                               const year = dtt.getFullYear() + '/';
                               const month = ('0' + (dtt.getMonth() + 1)).slice(-2) + '/';
                               const day = ('0' + dtt.getDate()).slice(-2);
-                            return(
-                    <DataTable.Row key={record.expense_ID} onPress = {()=> ViewExpense(record.expense_ID)}>
-                    <DataTable.Cell align = "center"> {record.expense_Type}</DataTable.Cell>
-                    <DataTable.Cell align = "center">Rs.{record.amount}.00</DataTable.Cell>
-                    <DataTable.Cell align = "center">{year+month+day}</DataTable.Cell>
-                    </DataTable.Row>
-                    )})
+
+                              if (record.status===0){
+                                return(
+                                  <DataTable.Row key={record.expense_ID} onPress = {()=> ViewExpense(record.expense_ID)}>
+                                  <DataTable.Cell align = "center"> {record.expense_Type}</DataTable.Cell>
+                                  <DataTable.Cell align = "center">Rs.{record.amount}.00</DataTable.Cell>
+                                  <DataTable.Cell align = "center">{year+month+day}</DataTable.Cell>
+                                  <DataTable.Cell numeric><Text style={{color:theme.colors.secondary, fontWeight: 'bold'}}>Pending</Text></DataTable.Cell>
+                                  </DataTable.Row>
+                                )                                
+                              } else if (record.status===1){
+                                return(
+                                  <DataTable.Row key={record.expense_ID} onPress = {()=> ViewExpense(record.expense_ID)}>
+                                  <DataTable.Cell align = "center"> {record.expense_Type}</DataTable.Cell>
+                                  <DataTable.Cell align = "center">Rs.{record.amount}.00</DataTable.Cell>
+                                  <DataTable.Cell align = "center">{year+month+day}</DataTable.Cell>
+                                  <DataTable.Cell numeric><Text style={{color:theme.colors.primary, fontWeight: 'bold'}}>Accepted</Text></DataTable.Cell>
+                                  </DataTable.Row>
+                                )  
+                              } else {
+                                return(
+                                  <DataTable.Row key={record.expense_ID} onPress = {()=> ViewExpense(record.expense_ID)}>
+                                  <DataTable.Cell align = "center"> {record.expense_Type}</DataTable.Cell>
+                                  <DataTable.Cell align = "center">Rs.{record.amount}.00</DataTable.Cell>
+                                  <DataTable.Cell align = "center">{year+month+day}</DataTable.Cell>
+                                  <DataTable.Cell numeric><Text style={{color:theme.colors.error, fontWeight: 'bold'}}>Rejected</Text></DataTable.Cell>
+                                  </DataTable.Row>
+                                )
+                              }
+                            })
                 }
 
                   <DataTable.Pagination
@@ -179,7 +277,8 @@ const styles = StyleSheet.create ({
     width : '100%'
   },
   card: {
-    backgroundColor :"#D2F7F7",
+    // backgroundColor :"#D2F7F7",
+    backgroundColor : theme.colors.surface,
     flex : 1,
     width : '100%',
     height : '80%',
@@ -206,8 +305,11 @@ const styles = StyleSheet.create ({
       fontSize : 16,
   },
   textLable : {
-      fontSize : 18,
-      marginBottom : 10,
+      marginLeft : -40,
+      fontSize : 16,
+      marginBottom : 6,
       fontWeight : 'bold',
+      color : theme.colors.primary,
+      // alignSelf : 'flex-start'
   },
 })
